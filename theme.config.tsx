6ad6bc2f6038e746ed39427a80d92022fa148761
DbgKinggg <samuelchen0606@gmail.com>
2024-01-10 15:18:03 +1100

    chore: add og image

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,6 +20,16 @@ const config: DocsThemeConfig = {
       <meta property="description" content="The all-in-one Web3 dashboard." />
       <meta property="og:title" content="Burokku" />
       <meta property="og:description" content="The all-in-one Web3 dashboard." />
+      <meta property="og:image" content="https://burokku.one/images/open-graph.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="2400" />
+      <meta property="og:image:height" content="1260" />
+      <meta name="twitter:title" content="Burokku"></meta>
+      <meta name="twitter:description" content="The all-in-one Web3 dashboard."></meta>
+      <meta name="twitter:image" content="https://burokku.one/images/open-graph.png" />
+      <meta name="twitter:image:type" content="image/png" />
+      <meta name="twitter:image:width" content="2400" />
+      <meta name="twitter:image:height" content="1260" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
