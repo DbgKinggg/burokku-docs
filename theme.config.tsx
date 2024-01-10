b216410d6eec039508e35b1ca0a31c96d0d4f29a
DbgKinggg <samuelchen0606@gmail.com>
2024-01-10 14:24:21 +1100

    feat: add icons and change app name

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span
-    style={{
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
-    }}
-  >Kazu</span>,
-  docsRepositoryBase: 'https://github.com/DbgKinggg/information-aggregator-docs',
+  logo: <Image
+    src="/icon-full-white.svg"
+    alt={'Burokku logo'}
+    width={126}
+    height={30}
+  />,
+  docsRepositoryBase: 'https://github.com/DbgKinggg/burokku-docs',
   footer: {
-    text: 'Kazu Documentation',
+    text: 'Burokku Documentation',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="title" content="Burokku" />
+      <meta property="description" content="The all-in-one Web3 dashboard." />
+      <meta property="og:title" content="Burokku" />
+      <meta property="og:description" content="The all-in-one Web3 dashboard." />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  ),
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Kazu'
+      titleTemplate: '%s – Burokku'
     }
   }
 }
